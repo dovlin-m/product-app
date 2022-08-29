37483a651460b7910a8856e625e535e491219bc4
dovlin_m <dovlin.m@gmail.com>
2022-08-29 20:27:17 +0300

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <MainPage />
+</template>
+
+<script>
+import MainPage from '../components/MainPage'
+
+export default {
+  name: 'IndexPage',
+  components: { MainPage }
+}
+</script>
